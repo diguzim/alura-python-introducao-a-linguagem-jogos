eaa43451590cc5f56adbfb585e5ea7e3107f5157
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2022-08-12 20:41:04 -0300

    Advancements within forca game: keeping track of found guesses

--- forca.py
+++ forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("Bem vindo ao jogo de Forca*******!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        posicao = 0
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                letras_acertadas[posicao] = letra
+            posicao += 1
+
+        print(letras_acertadas)
+
     print("Fim do jogo")
 
 
