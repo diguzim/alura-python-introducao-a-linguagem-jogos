a6138b40b0b95e90d8ea3fa09b29d33fb353ab92
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2022-08-15 10:52:11 -0300

    Improving logic when game is won/lost

--- forca.py
+++ forca.py
@@ -3,24 +3,37 @@ def jogar():
     print("Bem vindo ao jogo de Forca*******!")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while not enforcou and not acertou:
         chute = input("Digite uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        posicao = 0
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                letras_acertadas[posicao] = letra
-            posicao += 1
+        if chute in palavra_secreta:
+            posicao = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            print(f"Ops, você errou! Faltam {6 - erros} tentativas.")
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu")
     print("Fim do jogo")
 
 
